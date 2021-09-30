924729f0b4af0e89de253c16b3ce13384da10d24
Cintia Felix <cintiafelix1@gmail.com>
2021-09-30 10:16:35 -0300

    Calculadora Flex - Desafio JS

---
+++ calcJS/proj-calc-js/calc.js
@@ -0,0 +1,14 @@
+/* pesquisas apontam que só vale a pena abastecer com Álcool se o valor do litro dele for de no máximo 70% do valor da Gasolina, 
+pois os carros consomem 30% mais combustível no Álcool. */
+
+function calculadora() {
+  var alcool = parseFloat(document.getElementById("alcool").value);
+  var gasol = parseFloat(document.getElementById("gasol").value);    
+  var result = (alcool/gasol);
+  
+  if (result <= 0.7){
+    alert("Abasteça com alcool");
+  } else {
+    alert("Abasteça com gasolina");
+  }
+}
